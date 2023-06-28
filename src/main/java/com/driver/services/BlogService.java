4e1fb59eea4f8f271815be5a446d340047b4adb4
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2023-06-28 21:45:23 +0530

    first

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -23,11 +23,24 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-
+        User user = userRepository1.findById(userId).get();
+        Blog blog = new Blog();
+        blog.setUser(user);
+        blog.setTitle(title);
+        blog.setContent(content);
+        user.getBlogList().add(blog);
+        userRepository1.save(user);
+        return blog;
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        Blog blog = blogRepository1.findById(blogId).get();
+        User user = blog.getUser();
+        List<Blog> blogList = user.getBlogList();
+        blogList.remove(blog);
+        user.getBlogList().remove(blog);
 
+        userRepository1.save(user);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,52 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Blog blog = blogRepository2.findById(blogId).get();
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimension(dimensions);
+        image.setBlog(blog);
+        blog.getImageList().add(image);
 
+        blogRepository2.save(blog);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        Image image = imageRepository2.findById(id).get();
+        Blog blog = image.getBlog();
+        blog.getImageList().remove(image);
+        blogRepository2.save(blog);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image = imageRepository2.findById(id).get();
+        String description = image.getDescription();
+        int imageSize = 0;
+        int windowSize = 0;
 
+        String[] imageDesc = description.split("[X]", 0);
+        String[] screenDesc = screenDimensions.split("[X]", 0);
+
+        int imageL = StringToInt(imageDesc[0]);
+        int imageB = StringToInt(imageDesc[1]);
+        int imageArea = imageL*imageB;
+
+        int screenL = StringToInt(screenDesc[0]);
+        int screenB = StringToInt(screenDesc[1]);
+        int screenArea = screenB * screenL;
+
+        int cnt = screenArea/imageArea;
+        return cnt;
+    }
+
+    public int StringToInt(String s){
+        int res = 0;
+        for(int i=0; i<s.length(); i++){
+            int a = s.charAt(i) - '0';
+            res = res*10 + a;
+        }
+        return res;
     }
 }
